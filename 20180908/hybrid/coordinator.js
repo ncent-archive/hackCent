f17aa731b908b269bf8ae2f6e4a6daa49ff4643a
Dustin Pearson <dustinparkerpearson@gmail.com>
2018-09-08 14:56:07 -0700

    Scaffold coordinator structure; Track player referrals

--- 20180908/hybrid/coordinator.js
+++ 20180908/hybrid/coordinator.js
@@ -1,3 +1,8 @@
+const TelegramBot = require('node-telegram-bot-api');
+const TOKEN = require('./secret');
+const CHAT_ID = -1001318821764;
+const bot = new TelegramBot(TOKEN, {polling: true});
+
 class Player {
   constructor(ref) {
     this.ref = ref; // player object or id
@@ -14,43 +19,134 @@ class Player {
   }
 }
 
-class Coordinator {
-  constructor() {
-    // in-memory tracking of referrals
-    // in memory tracking of json
-    //
-    // stores all players in a hash with id -> player
-    // state that stores current step and votes
-  }
-
-  renderPoll() {
-    // renders poll of options
-  }
-
-  schedulePoll() {
-
-  }
-
-  updateReferralChain(player) {
-    // updates referral chain of players leading up to
-  }
-
-  storeSubmittedResponse() {
-
-  }
-
-  requestSubmissions() {
-
-  }
-
-  scheduleRequestForSubmissions() {
-
-  }
-  
-  memberAdded() {
-    // track
-    // invoke updateReferralChain on player
-  }
-
-
+async function isAdmin(authorId) {
+  const sender = await bot.getChatMember(CHAT_ID, authorId);
+  const status = sender.status
+  return (status === 'creator' || status === 'administrator');
 }
+
+class Coordinator {
+  constructor(coordinatorOpt) {
+    this.options = coordinatorOpt; // stores steps data, reward amount, etc.
+    // Store reward amount from options
+    this.players = {}; // id -> player
+    this.submissions = {}; // id -> submission
+    this.playerSubmissions = {}; // joins table between player and submissions
+    this.active_steps = [];
+    this.active_poll_id = 0;
+  }
+
+  // renderPoll(pollOpt) {
+  //   // renders poll of options
+  //   /*
+  //     var option = {
+  //                   "parse_mode": "Markdown",
+  //                   "reply_markup":
+  //                 };
+  //     bot.sendMessage(msg.chat.id, "*Some* message here.", option);
+  //   */
+  //   const replyMarkup = { "keyboard": [["Yes"],["No"]] };
+  //   const text = "Do you want to answer my question?";
+  //   bot.sendMessage(CHAT_ID, text, {
+  //                                     "parse_mode": 'Markdown',
+  //                                     "reply_markup": replyMarkup
+  //                                   })
+  //   .then((msg)=>{
+  //     this.active_poll_id = msg.message_id;
+  //   });
+  // }
+
+  // schedulePoll(timeFromNow, pollOpt) {
+  //   setTimeout(()=>{
+  //     this.renderPoll(pollOpt);
+  //   }, timeFromNow);
+  // }
+
+  updatePointsOfReferralChain(player) {
+    let currentPlayer = player;
+    while (player.ref !== -1) {
+      // do some point action
+      currentPlayer = this.players[currentPlayer.ref];
+    }
+  }
+
+  onSubmittedResponse(response) {
+    // this is already verified as a response to one of the active_steps
+  }
+
+  requestSubmissions(submissionDetails) {
+    const title = submissionDetails['title'];
+    this.active_steps.push(title);
+    // TODO change submissionRequestText to correct param
+    bot.sendMessage(CHAT_ID, submissionDetails['submissionRequestText']);
+  }
+
+  endSubmissions(submissionDetails) {
+    const title = submissionDetails['title'];
+    const index = this.active_steps.indexOf(title);
+    this.active_steps.splice(index, 1);
+    bot.sendMessage(CHAT_ID, `We have completed accepting submissions for ${title}`)
+  }
+
+  scheduleRequestForSubmissions(timeFromNow, requestOpt) {
+    setTimeout(()=>{
+      this.requestSubmissions(requestOpt);
+      setTimeout(()=>{
+        this.endSubmissions(requestOpt);
+      }, 6000);
+    }, timeFromNow);
+  }
+
+  onMemberAdded(msg) {
+    if (msg.from.id === msg.new_chat_members[0].id) {// Player joined without invite
+      const plr = new Player(-1);
+      const id = msg.from.id;
+      this.players[id] = plr;
+    } else {
+      const referrer = msg.from.id;
+      for (let i = 0; i < msg.new_chat_members.length; i++) {
+        const plr = new Player(referrer);
+        const id = msg.new_chat_members[i].id;
+        this.players[id] = plr;
+      }
+    }
+  }
+
+  hookMessage() {
+    bot.on('message', (msg)=>{
+      console.log(msg);
+      if ('new_chat_members' in msg) {
+        this.onMemberAdded(msg);
+      } else if ('text' in msg) {
+        for (let i = 0; i < this.active_steps.length; i++) {
+          const title = this.active_steps[i];
+          if (msg.text[0] === '#' && (msg.substr(1, title.length) === title)) {
+            this.onSubmittedResponse(msg);
+          }
+        }
+        if (isAdmin(msg.from.id)) {
+          if (msg.text.slice(0, 14) == '/choose_winner') {
+            const username = msg.text.slice(15);
+
+          }
+        }
+        // POLL CODE
+        // if ('reply_to_message' in msg && ) {
+        //   bot.sendMessage(CHAT_ID, 'thanks');
+        //   // check if message is a reply to the poll
+        // }
+      }
+    });
+  }
+
+  beginProject() {
+    this.hookMessage();
+    // schedules all request for submissions
+    // schedule polls
+  }
+}
+
+
+const coord = new Coordinator({});
+coord.renderPoll();
+coord.beginProject();
