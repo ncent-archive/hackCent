4ac40f285447bedda249ded9e0e15d55da594ea2
15chrjef <15chrjef@gmail.com>
2018-09-08 18:52:00 -0700

    (feature) implement transfer of funds and persistent users

--- 20180908/search/jobBot.js
+++ 20180908/search/jobBot.js
@@ -1,6 +1,13 @@
 const TelegramBot = require('node-telegram-bot-api');
 const nCentSDK = require('ncent-sandbox-sdk');
+const stellarSDK = require('stellar-sdk')
+const utils = require('./db_utils');
+
 sdk = new nCentSDK();
+utils.set_up_users_table()
+.then(result => {
+  console.log('result', result)
+})
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TOKEN;
@@ -12,15 +19,62 @@ const generate_key_pair = () => {
   return sdk.createWalletAddress()
 }
 
-let current_address = null
+const stamp_tokens = (user, numbers) => {
+  utils.get_user(user)
+  .then(result => {
+    new Promise(function(resolve, reject) {
+      sdk.stampToken(result.publickey, 'jeffjobCents' + Math.random().toString(), numbers[0], '2021', resolve, reject)
+    })
+    .then(result => console.log('result', result))
+  })
+  .catch(err => {
+    console.log('err', err)
+  })
+}
+
+const get_all_balances = (user, chatId) => {
+  utils.get_user(user)
+  .then(result => {
+    new Promise(function(resolve, reject) {
+      sdk.getAllBalances(result.publickey, resolve, reject)
+    })
+    .then((result) => {
+      const balances = result.data
+      balances.forEach(balance => {
+        bot.sendMessage(chatId, 'You have ' + balance.balance + ' of ' + balance.uuid)
+      })
+    })
+  })
+  .catch((err) => { console.log('error', err) })
+}
+
+const send_to_user = (user, address_to_recieve, amount) => {
+  key_pair = null
+  utils.get_user(user)
+  .then(result => {
+    key_pair = stellarSDK.Keypair.fromSecret(result.privatekey)
+    new Promise(function(resolve, reject) {
+      sdk.getAllBalances(result.publickey, resolve, reject)
+    })
+    .then((result) => {
+      const balances = result.data
+      currency_id = balances[0].uuid
+      new Promise(function(resolve, reject) {
+        sdk.transferTokens(key_pair, address_to_recieve, currency_id, amount, resolve, reject)
+      })
+      .then(res => console.log('res', res))
+      .catch(err => console.log('err', err))
+    })
+  })
+  .catch((err) => { console.log('error', err) })
+}
 
 const sendInitialDialogue = (user, chatId) => {
   bot.sendMessage(chatId, `Hello and Welcome ` + user);
   bot.sendMessage(chatId, `- To create a new Wallet say 'new wallet'`);
-  bot.sendMessage(chatId, `- To view an account say 'use x' where x is the public address`)
   bot.sendMessage(chatId, `- To view token balances say 'view balances'`)
   bot.sendMessage(chatId, `- To add jobcent tokens to your account say 'Can I have x jobcents' where x is the number of tokens (REQUIRES ADDRESS BEING SET)`);
-  // bot.sendMessage(chatId, `- To transfer your jobcent tokens to another account say 'Transfer x jobcents to y' where x is the number of tokens and y is the address you want to send to(REQUIRES ADDRESS BEING SET)`);
+  bot.sendMessage(chatId, `- To transfer your jobcent tokens to another account say 'Transfer x jobcents to y' where x is the number of tokens and y is the address you want to send to(REQUIRES ADDRESS BEING SET)`);
 }
 
 // Listen for any kind of message. There are different kinds of
@@ -29,47 +83,30 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const txt = msg.text;
   const user = msg.from.username
-
   const is_ask = txt.toString().toLowerCase().includes('can i have')
   const includes_jobcent = txt.toString().toLowerCase().includes('jobcent')
   const wants_new_wallet = txt.toString().toLowerCase() === 'new wallet'
   const wants_to_view_balances = txt.toString().toLowerCase() === 'view balances'
-  const should_use_wallet = txt.toString().includes('use G')
+  const should_transfer = txt.toString().toLowerCase().includes('transfer')
+
   const is_ask_of_payment = is_ask && includes_jobcent
 
-  if (current_address && is_ask_of_payment) {
+  if (is_ask_of_payment) {
     const numbers = txt.match(/\d+/g).map(Number);
     bot.sendMessage(chatId, 'Sure, sending ' + numbers + ' jobcents')
-    new Promise(function(resolve, reject) {
-      return sdk.stampToken(current_address, 'jeffjobCents' + Math.random().toString(), numbers[0], '2021', resolve, reject)
-    })
-    .then((res) => {
-      console.log('res', res)
-    })
-    .catch((err) => {
-      console.log('err', err)
-    })
+    stamp_tokens(user, numbers)
   } else if (wants_new_wallet) {
     const wallet = generate_key_pair()
     bot.sendMessage(chatId, 'Sure, your new wallet address is ' + wallet.publicKey()  + ' make sure not to lose it :)')
-    current_address = wallet.publicKey()
-  } else if (current_address && wants_to_view_balances) {
-    new Promise(function(resolve, reject) {
-      sdk.getAllBalances(current_address, resolve, reject)
-    })
-    .then((result) => {
-      const balances = result.data
-      balances.forEach(balance => {
-        bot.sendMessage(chatId, 'You have ' + balance.balance + ' of ' + balance.uuid)
-      })
-    })
-    .catch((err) => {
-      console.log('error', err)
-    })
-
-  } else if (should_use_wallet) {
-    current_address = txt.slice(4)
-    bot.sendMessage(chatId, 'Using address ' + current_address)
+    utils.insert_user(user, wallet.publicKey(), wallet.secret())
+  } else if (wants_to_view_balances) {
+    get_all_balances(user, chatId)
+  } else if (should_transfer) {
+    const amount_to_transfer = txt.match(/\d+/g).map(Number)[0];
+    const start_of_public_key = txt.indexOf('G')
+    const public_key = txt.slice(start_of_public_key)
+    bot.sendMessage(chatId, 'Sure, just sent ' + amount_to_transfer + ' jobcents to ' + public_key)
+    send_to_user(user, public_key, amount_to_transfer)
   } else {
     sendInitialDialogue(user, chatId)
   } 
